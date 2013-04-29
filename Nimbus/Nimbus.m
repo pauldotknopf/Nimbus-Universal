1cfc54391a414820587d3bc26d278a3708fec15e
Paul Knopf <theonlylawislove@gmail.com>
2013-04-29 13:31:39 -0400

    Adding initial universal library project

---
+++ Nimbus/Nimbus.m
@@ -0,0 +1,13 @@
+//
+//  Nimbus.m
+//  Nimbus
+//
+//  Created by Paul Knopf on 4/29/13.
+//  Copyright (c) 2013 Paul Knopf. All rights reserved.
+//
+
+#import "Nimbus.h"
+
+@implementation Nimbus
+
+@end
