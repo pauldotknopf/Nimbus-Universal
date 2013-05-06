f1d4ce899180b1ae6d017b8c826298a62d892d61
Paul Knopf <theonlylawislove@gmail.com>
2013-05-05 22:00:34 -0400

    Adding a .m file to generate a processed header file for the entire solution. This is so that I can generate the MonoTouch bindings.

---
+++ Nimbus/Preprocessed.m
@@ -0,0 +1,24 @@
+//
+//  Preprocessed.c
+//  Nimbus
+//
+//  Created by Paul Knopf on 5/5/13.
+//  Copyright (c) 2013 Paul Knopf. All rights reserved.
+//
+
+
+#import "NimbusCore.h"
+#import "NimbusWebController.h"
+#import "NITextField.h"
+#import "NimbusPhotos.h"
+#import "NimbusOverview.h"
+#import "NimbusNetworkImage.h"
+#import "NimbusNetworkControllers.h"
+#import "NimbusModels.h"
+#import "NimbusInterapp.h"
+#import "NimbusCollections.h"
+#import "NimbusBadge.h"
+#import "NimbusCSS.h"
+#import "NimbusAttributedLabel.h"
+#import "NimbusLauncher.h"
+#import "NimbusPagingScrollView.h"
